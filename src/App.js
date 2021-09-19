ebcd2f2643b471aea82c8ff6a833439c56a8b72c
Sam Guan <xin.guan@gmail.com>
2021-09-18 17:20:50 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { API }  from 'aws-amplify';
+import React, { useEffect, useState} from 'react'
 
 function App() {
+  useEffect(() => {
+    fetchWoohoos()
+  }, [])
+
+  const [myMessage, setMyMessage] = useState('')
+
+  async function fetchWoohoos() {
+
+    API
+      .get("myLamAPI", "/woohoo", {})
+        .then(response => {
+          setMyMessage(response.success)
+          console.log(`Response: ${response}`)
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +37,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>{myMessage}</p>
       </header>
     </div>
   );
